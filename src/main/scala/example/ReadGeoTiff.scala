7760d0bc4af119e2c1de54d9803899f34160ba93
niujp08@qq.com <niujp08@qq.com>
2019-11-05 10:58:15 +0800

    init

--- src/main/scala/example/ReadGeoTiff.scala
+++ src/main/scala/example/ReadGeoTiff.scala
@@ -7,7 +7,7 @@ import geotrellis.proj4.LatLng
 import geotrellis.raster.Tile
 import geotrellis.raster.io.geotiff._
 import geotrellis.raster.io.geotiff.reader.GeoTiffReader
-import geotrellis.raster.render.{ColorMap, ColorRamps}
+import geotrellis.raster.render.{ColorMap, ColorRamp, ColorRamps}
 
 object ReadGeoTiff {
   val path: String = "D:/data/gis_data/dem/beijing.tif"
@@ -25,10 +25,9 @@ object ReadGeoTiff {
     // tile2.mapDouble { z => z + 1.1 } // Map over double values.
     // tile1.dualMap({ z => z + 1 })({ z => z + 1.1 }) // Call either the integer value or double version, depending on cellType.
     // tile1.combine(tile2) { (z1, z2) => z1 + z2 }
-    val colorMap = ColorMap(
-      (0 to tile1.findMinMax._2 by 8).toArray,
-      ColorRamps.HeatmapBlueToYellowToRedSpectrum
-    )
+    val colorMap = ColorRamp(0xFF0000FF, 0x0000FFFF)
+      .stops(10)
+      .toColorMap(tile.histogram)
     val pathOut: String = "D:\\lzugis19\\code\\geotrellis-test\\result\\test.png"
     tile.renderPng(colorMap).write(pathOut)
     val pathBase: String = "D:/data/gis_data/dem/"
